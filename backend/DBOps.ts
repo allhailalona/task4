a536a199d4d6ae6ceeefad7fa8779f600c951d18
Your Name <you@example.com>
2024-12-10 15:41:33 +0200

    check

--- backend/DBOps.ts
+++ backend/DBOps.ts
@@ -1,24 +1,6 @@
 import mongoose from 'mongoose'
-
-const TaskSchema = new mongoose.Schema({
-  _id: Number, 
-  description: String, 
-  dateCreated: Date, 
-  client_id: Number, 
-  status: Boolean
-})
-
-const TaskModel = mongoose.model('Task', TaskSchema, 'tasks')
-
-const ClientSchema = new mongoose.Schema({
-  _id: Number, 
-  name: String, 
-  profession: String, 
-  phone: Number,
-  email: String
-})
-
-const ClientModel = mongoose.model('Client', ClientSchema,'clients')
+import { Client } from './models/Client'
+import { Task } from './models/Task'
 
 export async function connectDB() {
   try {
@@ -33,7 +15,7 @@ export async function fetchTasks() {
   try {
     await connectDB()
 
-    const tasks = await TaskModel.find({})
+    const tasks = await Task.find({})
     return tasks
   } catch (err) {
     console.error('Error in fetchTasks DBOp', err)
@@ -46,7 +28,7 @@ export async function fetchClients() {
     await connectDB()
 
     console.log('about to fetch clients')
-    const clients = await ClientModel.find({})
+    const clients = await Client.find({})
     return clients
   } catch (err) {
     console.error('Error in fetchClients DBOp', err)
--- backend/initDB.ts
+++ backend/initDB.ts
@@ -1,21 +1,15 @@
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+import { Client } from './models/Client'
+import { Task } from './models/Task'
 
-// Task Schema
-const taskSchema = new mongoose.Schema({
- id: Number,
- description: String,
- dateCreated: Date,
- clientId: Number,
- status: Boolean
-});
-
-// Create model with collection name
-const Task = mongoose.model('Task', taskSchema, 'tasks');
+dotenv.config()
 
 // Add authentication options
+console.log('about to connect uri is', process.env.MONGODB_URI)
 mongoose.connect(process.env.MONGODB_URI); 
 
-// Fun sample data
+// Tasks data
 const tasks = [
  {id: 1, description: "Teach penguins to code JavaScript", dateCreated: new Date(), clientId: 101, status: false},
  {id: 2, description: "Debug unicorn's rainbow generator", dateCreated: new Date(), clientId: 102, status: true},
@@ -29,7 +23,26 @@ const tasks = [
  {id: 10, description: "Scale up yeti's snowflake microservice", dateCreated: new Date(), clientId: 110, status: true}
 ];
 
+// Clients data
+const clients = [
+  {_id: 101, name: "Dracula von Byte", profession: "Blood Type Analyst", phone: 5551234567, email: "fang.master@nightcode.com"},
+  {_id: 102, name: "Rainbow Sparklecloud", profession: "Unicorn UI Designer", phone: 5552345678, email: "sparkles@magictech.com"},
+  {_id: 103, name: "Inferno Scale", profession: "Heat Management Engineer", phone: 5553456789, email: "hotcode@dragondev.com"},
+  {_id: 104, name: "Marina Echo", profession: "Underwater Sound Architect", phone: 5554567890, email: "singing@deepsea.dev"},
+  {_id: 105, name: "Zom B. Walker", profession: "Neural Network Specialist", phone: 5555678901, email: "brains@undeadtech.com"},
+  {_id: 106, name: "Merlin Debugger", profession: "Legacy Code Wizard", phone: 5556789012, email: "spells@wizardware.com"},
+  {_id: 107, name: "Shadow Exception", profession: "Stealth Code Ninja", phone: 5557890123, email: "hidden@ninjacorp.com"},
+  {_id: 108, name: "Zorg Overflow", profession: "Intergalactic Developer", phone: 5558901234, email: "alien@spacecode.com"},
+  {_id: 109, name: "Count Query", profession: "Database Vampire", phone: 5559012345, email: "nosql@vampiredb.com"},
+  {_id: 110, name: "Frost Callback", profession: "Cloud Computing Yeti", phone: 5550123456, email: "snow@yetidev.com"}
+];
+
 // Insert data
 Task.insertMany(tasks)
  .then(() => console.log('Tasks inserted'))
  .catch(err => console.error(err));
+
+// Insert client data
+Client.insertMany(clients)
+  .then(() => console.log('Clients inserted'))
+  .catch(err => console.error(err));
